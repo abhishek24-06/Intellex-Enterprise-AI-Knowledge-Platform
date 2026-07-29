6a484fd980b722ad916fc367073108709513dbb8
abhishek24-06 <tajaneabhishek5@gmail.com>
2026-07-29 17:41:17 +0530

    Add DocumentChunk model and migration; update document relationships

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -6,4 +6,5 @@ from app.models.documents import Document
 from app.models.chat_session import ChatSession
 from app.models.chat_history import ChatHistory
 from app.models.document_acl import DocumentACL
+from app.models.document_chunks import DocumentChunk
 
--- app/models/document_chunks.py
+++ app/models/document_chunks.py
@@ -1,9 +1,9 @@
-from app.enums.enums import EmbeddingStatus
 from app.models.base import Base
-from sqlalchemy import Integer,String,ForeignKey,Boolean,DateTime,UniqueConstraint,Enum,JSON
+from sqlalchemy import Integer,String,ForeignKey,DateTime,UniqueConstraint,JSON,Text
 from sqlalchemy.orm import mapped_column,Mapped,relationship
 from datetime import UTC, datetime
 from typing import TYPE_CHECKING
+
 
 if TYPE_CHECKING:
     from app.models.documents import Document
@@ -13,7 +13,7 @@ class DocumentChunk(Base):
 
     __table_args__=(
         UniqueConstraint(
-            "documenr_id",
+            "document_id",
             "chunk_index",
             name="uq_document_chunk_index"
         ),
@@ -23,20 +23,20 @@ class DocumentChunk(Base):
 
     document_id:Mapped[int]=mapped_column(ForeignKey("documents.document_id",ondelete="CASCADE"),nullable=False)
 
-    chunk_index:Mapped[int]=mapped_column(nullable=True)
+    chunk_index:Mapped[int]=mapped_column(nullable=False)
 
-    chunk_text:Mapped[str]=mapped_column(String(500),nullable=False)
+    chunk_text:Mapped[str]=mapped_column(Text,nullable=False)
 
     token_count:Mapped[int]=mapped_column(Integer,nullable=False)
 
-    vector_id:Mapped[str|None]=mapped_column(String(255),unique=True,nullable=False)
+    vector_id:Mapped[str|None]=mapped_column(String(255),unique=True,nullable=True)
 
-    metadata_json:Mapped[dict|None]=mapped_column(JSON,nullable=True)
+    metadata_json:Mapped[dict|None]=mapped_column(JSON,nullable=False)
 
     created_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),default=lambda:datetime.now(UTC))
 
     updated_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),default=lambda:datetime.now(UTC),onupdate=lambda:datetime.now(UTC))
 
     #RELATIONSHIPS
-    documents:Mapped["Document"]=relationship(back_populates="chunks")
+    document:Mapped["Document"]=relationship(back_populates="chunks")
 
